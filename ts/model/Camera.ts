2291988be379db534a74964d6a3258ec127f61d9
Lucas Lelievre <lucas.lelievre@capgemini.com>
2020-02-21 13:46:00 +0100

    re written camera class

---
+++ ts/model/Camera.ts
@@ -0,0 +1,14 @@
+class Camera {
+
+    private dir: vec3;
+    private pos: vec3;
+
+    constructor(pos: vec3, dir: vec2) {
+        this.dir = dir;
+        this.pos = pos;
+    }
+
+    public update(heys: string[]): void {
+
+    }
+}
