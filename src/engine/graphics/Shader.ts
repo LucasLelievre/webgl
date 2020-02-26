f6110cc1cef156919e91db37a0ecc2ede8dde589
Lucas Lelievre <lucas.lelievre@capgemini.com>
2020-02-26 11:55:43 +0100

    wrote the shader class

--- src/engine/graphics/Shader.ts
+++ src/engine/graphics/Shader.ts
@@ -2,10 +2,15 @@ class Shader {
 
     private program: WebGLProgram;
 
+    private vertexPosition: number;
+    private vertexColor: number;
+    private projectionMatrix: WebGLUniformLocation;
+    private modelViewMatrix: WebGLUniformLocation;
+
     private vertexShader: string;
     private fragmentShader: string;
 
-    constructor(pathV: string, pathS: string, ){
+    constructor(pathV: string, pathS: string, ) {
         //this.readVertexShader("./vertexShader.txt");
         //TODO read the shaders from files
         this.vertexShader = `
@@ -31,6 +36,39 @@ class Shader {
             }`;
 
         this.program = this.initShaderProgram(Main.getGlContext(), this.vertexShader, this.fragmentShader) as WebGLProgram;
+
+        this.vertexPosition = Main.getGlContext().getAttribLocation(this.program, 'aVertexPosition');
+        this.vertexColor = Main.getGlContext().getAttribLocation(this.program, 'aVertexColor');
+        this.projectionMatrix = Main.getGlContext().getUniformLocation(this.program, 'uProjectionMatrix') as WebGLUniformLocation;
+        this.modelViewMatrix = Main.getGlContext().getUniformLocation(this.program, 'uModelViewMatrix') as WebGLUniformLocation;
+    }
+
+    /**
+     * return the vertex position attribute's location
+     */
+    public getVertexPosLocation(): number {
+        return this.vertexPosition;
+    }
+
+    /**
+     * return the vertex colour attribute's location 
+     */
+    public getVertexColorLocation(): number {
+        return this.vertexColor;
+    }
+
+    /**
+     * return the projection matrix uniform's location
+     */
+    public getProjectionMatrixLocation(): WebGLUniformLocation {
+        return this.projectionMatrix;
+    }
+
+    /**
+     * return the model view matric uniform's location
+     */
+    public getModelViewMatrixLocation(): WebGLUniformLocation {
+        return this.modelViewMatrix;
     }
 
     /**
@@ -39,7 +77,7 @@ class Shader {
      * @param vsSource vertex shader code
      * @param fsSource fragment shader code
      */
-    private initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string) {
+    private initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string): WebGLProgram | null {
         const vertexShader = this.loadShader(gl, gl.VERTEX_SHADER, vsSource) as WebGLShader;
         const fragmentShader = this.loadShader(gl, gl.FRAGMENT_SHADER, fsSource) as WebGLShader;
 
@@ -58,24 +96,30 @@ class Shader {
         return shaderProgram;
     }
 
-    private loadShader(gl: WebGLRenderingContext, type: number, source: string) {
+    /**
+     * Load shader code in the webgl rendering context
+     * @param gl webgl rendering context
+     * @param type shader type
+     * @param source shader code
+     */
+    private loadShader(gl: WebGLRenderingContext, type: number, source: string): WebGLShader | null {
         const shader = gl.createShader(type) as WebGLShader;
-      
+
         // Send the source to the shader object
         gl.shaderSource(shader, source)
-      
+
         // Compile the shader program
         gl.compileShader(shader);
-      
+
         // See if it compiled successfully
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-          alert('An error occured compiling the shaders: ' + gl.getShaderInfoLog(shader));
-          gl.deleteShader(shader);
-          return null;
+            alert('An error occured compiling the shaders: ' + gl.getShaderInfoLog(shader));
+            gl.deleteShader(shader);
+            return null;
         }
-      
+
         return shader;
-      }
+    }
 
     /*public readVertexShader(path: string): void {
         //TODO read file and store it in string
