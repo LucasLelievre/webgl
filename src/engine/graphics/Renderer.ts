5543e4135b35f7c0efc5b5e68169ff1daa62406f
Lucas Lelievre <lucas.lelievre@capgemini.com>
2020-02-26 17:41:58 +0100

    Added a shader to the renderer + using the static glContext

--- src/engine/graphics/Renderer.ts
+++ src/engine/graphics/Renderer.ts
@@ -2,14 +2,16 @@ import { mat4 } from "../maths/gl-matrix"
 
 class Renderer {
 
-    private glContext: WebGLRenderingContext;
-    private programInfo: object;
+    //private glContext: WebGLRenderingContext;
+    //private programInfo: object;
+    private shader: Shader;
 
     private projectionMatrix: mat4;
 
-    constructor(glContext: WebGLRenderingContext, programInfo: object) {
-        this.glContext = glContext;
-        this.programInfo = programInfo;
+    constructor(/*glContext: WebGLRenderingContext, programInfo: object*/) {
+        //this.glContext = glContext;
+        //this.programInfo = programInfo;
+        this.shader = new Shader();
 
         this.projectionMatrix = mat4.create();
     }
@@ -17,7 +19,7 @@ class Renderer {
     public resize(): void {
         mat4.perspective(this.projectionMatrix,                             // out matrix
             45 * Math.PI / 180,                                             // field of view in radians
-            this.glContext.canvas.width / this.glContext.canvas.height,     // aspect ratio
+            Main.getGlContext().canvas.width / Main.getGlContext().canvas.height,     // aspect ratio
             0.1,                                                            // z near
             100.0)                                                          // z far
     }
@@ -26,67 +28,67 @@ class Renderer {
         // Tell WebGL how to pull out the positions from the position buffer into the vertexPosition attribute
         {
             const numComponents = 3;  // pull out 2 values per iteration
-            const type = this.glContext.FLOAT;    // the data in the buffer is 32bit floats
+            const type = Main.getGlContext().FLOAT;    // the data in the buffer is 32bit floats
             const normalize = false;  // don't normalize
             const stride = 0;         // how many bytes to get from one set of values to the next
             // 0 = use type and numComponents above
             const offset = 0;         // how many bytes inside the buffer to start from
-            this.glContext.bindBuffer(this.glContext.ARRAY_BUFFER, mesh.getPositions());
-            this.glContext.vertexAttribPointer(
-                this.programInfo.attribLocations.vertexPosition,
+            Main.getGlContext().bindBuffer(Main.getGlContext().ARRAY_BUFFER, mesh.getPositions());
+            Main.getGlContext().vertexAttribPointer(
+                this.shader.getVertexPosLocation(),
                 numComponents,
                 type,
                 normalize,
                 stride,
                 offset,
             );
-            this.glContext.enableVertexAttribArray(
-                this.programInfo.attribLocations.vertexPosition,
+            Main.getGlContext().enableVertexAttribArray(
+                this.shader.getVertexPosLocation(),
             );
         }
 
         // Tell WebGL how to pull out the colors from the color buffer into the vertexcolor attributer
         {
             const numComponents = 4;
-            const type = this.glContext.FLOAT;
+            const type = Main.getGlContext().FLOAT;
             const normalize = false;
             const stride = 0;
             const offset = 0;
-            this.glContext.bindBuffer(this.glContext.ARRAY_BUFFER, mesh.getColours());
-            this.glContext.vertexAttribPointer(
-                this.programInfo.attribLocations.vertexColor,
+            Main.getGlContext().bindBuffer(Main.getGlContext().ARRAY_BUFFER, mesh.getColours());
+            Main.getGlContext().vertexAttribPointer(
+                this.shader.getVertexColorLocation(),
                 numComponents,
                 type,
                 normalize,
                 stride,
                 offset,
             );
-            this.glContext.enableVertexAttribArray(
-                this.programInfo.attribLocations.vertexColor
+            Main.getGlContext().enableVertexAttribArray(
+                this.shader.getVertexColorLocation()
             );
         }
 
         // Tell WebGL which indices to use to index the vertices
-        this.glContext.bindBuffer(this.glContext.ELEMENT_ARRAY_BUFFER, mesh.getIndices());
+        Main.getGlContext().bindBuffer(Main.getGlContext().ELEMENT_ARRAY_BUFFER, mesh.getIndices());
 
         // Tell WebGL to use our program when drawing
-        this.glContext.useProgram(this.programInfo.program);
+        Main.getGlContext().useProgram(this.shader.getProgram());
 
         // Set the shader uniforms
-        this.glContext.uniformMatrix4fv(
-            this.programInfo.uniformLocation.projectionMatrix,
+        Main.getGlContext().uniformMatrix4fv(
+            this.shader.getProjectionMatrixLocation(),
             false,
             this.projectionMatrix);
-        this.glContext.uniformMatrix4fv(
-            this.programInfo.uniformLocation.modelViewMatrix,
+            Main.getGlContext().uniformMatrix4fv(
+            this.shader.getModelViewMatrixLocation(),
             false,
             modelViewMatrix);
 
         {
-            const vertexCount = mesh.getVertexCount();//TODO mesh's vertex count
-            const type = this.glContext.UNSIGNED_SHORT;
+            const vertexCount = mesh.getVertexCount();
+            const type = Main.getGlContext().UNSIGNED_SHORT;
             const offset = 0;
-            this.glContext.drawElements(this.glContext.TRIANGLES, vertexCount, type, offset);
+            Main.getGlContext().drawElements(Main.getGlContext().TRIANGLES, vertexCount, type, offset);
         }
     }
 }
