2625af396922556b686b7814366028407d6969f1
Lucas Lelievre <lucas.lelievre@capgemini.com>
2020-03-03 14:03:02 +0100

    deleted old shader functions + fix error in main

--- src/engine/main.ts
+++ src/engine/main.ts
@@ -20,7 +20,7 @@ class Main {
 
         this.oldTime = 0.0;
         this.deltaTime = 0.0;
-        requestAnimationFrame(this.update);
+        requestAnimationFrame(this.update.bind(this));
     }
 
     public static getCanvas(): HTMLCanvasElement {
@@ -30,52 +30,6 @@ class Main {
     public static getGlContext(): WebGLRenderingContext {
         return Main.glContext as WebGLRenderingContext;
     }
-
-    /**
-     * Initialize a shader program, so WebGL knows how to draw our data
-     * @param gl webgl rendering context
-     * @param vsSource vertex shader code
-     * @param fsSource fragment shader code
-     */
-    private initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string) {
-        const vertexShader = this.loadShader(gl, gl.VERTEX_SHADER, vsSource) as WebGLShader;
-        const fragmentShader = this.loadShader(gl, gl.FRAGMENT_SHADER, fsSource) as WebGLShader;
-
-        // Create the shader program
-        const shaderProgram = gl.createProgram() as WebGLProgram;
-        gl.attachShader(shaderProgram, vertexShader);
-        gl.attachShader(shaderProgram, fragmentShader);
-        gl.linkProgram(shaderProgram);
-
-        // If creating the shader program failed, alert
-        if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-            alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-            return null;
-        }
-
-        return shaderProgram;
-    }
-
-    private loadShader(gl: WebGLRenderingContext, type: number, source: string) {
-        const shader = gl.createShader(type) as WebGLShader;
-      
-        // Send the source to the shader object
-        gl.shaderSource(shader, source)
-      
-        // Compile the shader program
-        gl.compileShader(shader);
-      
-        // See if it compiled successfully
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-          alert('An error occured compiling the shaders: ' + gl.getShaderInfoLog(shader));
-          gl.deleteShader(shader);
-          return null;
-        }
-      
-        return shader;
-      }
-
-
 
     private initEvents(): void {
         Main.canvas.addEventListener("mousedown", (e) => this.gameWorld.getMouse().mouseDown(e), false);
@@ -98,7 +52,7 @@ class Main {
         this.render();
 
         // Call a new update and frame
-        requestAnimationFrame(this.update);
+        requestAnimationFrame(this.update.bind(this));
     }
 
     private render(): void {
