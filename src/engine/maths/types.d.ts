aae62fabea467ba4afd1bcea60d4288fa2e9db01
LucasLelievre <lucas.lelievre98@gmail.com>
2020-03-08 00:43:54 +0100

    small updates to the methods parameters names

--- src/engine/maths/types.d.ts
+++ src/engine/maths/types.d.ts
@@ -4,7 +4,7 @@ declare class mat4 {
   static identity(out: Float32List): void;
   static translate(out: Float32List, int: Float32List, pos: Float32Array): void;
   static scale(out: Float32List, int: Float32List, size: Float32Array): void;
-  static rotate(out: Float32List, int: Float32List, angle: number, axis: Float32Array): void;
+  static rotate(out: Float32List, int: Float32List, radian: number, axis: Float32Array): void;
 }
 
 declare class vec2 {
@@ -21,6 +21,6 @@ declare class vec3 {
   static set(out: Float32Array, x: number, y: number, z: number): void;
   static add(out: Float32Array, vec3in: Float32Array, vec3add: Float32Array): void;
   static subtract(out: Float32Array, vec3in: Float32Array, vec3add: Float32Array): void;
-  static rotateX(out: Float32Array, input: Float32Array, origin: Float32Array, rad: number): void;
-  static rotateY(out: Float32Array, input: Float32Array, origin: Float32Array, rad: number): void;
+  static rotateX(out: Float32Array, input: Float32Array, origin: Float32Array, radian: number): void;
+  static rotateY(out: Float32Array, input: Float32Array, origin: Float32Array, radian: number): void;
 }
