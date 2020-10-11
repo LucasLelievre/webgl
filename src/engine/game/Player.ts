c61757ea71d97cb8be94ec77205add03798d2084
lucas lelievre <lucas.lelievre98@gmail.com>
2020-10-11 17:24:30 +0200

    Added player speed

--- src/engine/game/Player.ts
+++ src/engine/game/Player.ts
@@ -1,6 +1,7 @@
 class Player extends Entity {
 
     private hp = {value: 3};
+    private speed = 0.1;
 
     constructor(pos: Float32Array, dir: Float32Array) {
         super(pos, dir, vec3.fromValues(1.0, 1.0, 1.0), Mesh.getPlayerMesh());
@@ -17,13 +18,13 @@ class Player extends Entity {
         // Update the entity
         //TODO movement based on keys
         if (keys.indexOf(Keyboard.left) != -1)
-            vec3.add(this.getPos(), this.getPos(), vec3.fromValues(0.1, 0.0, 0.0)); // left
+            vec3.add(this.getPos(), this.getPos(), vec3.fromValues(this.speed, 0.0, 0.0)); // left
         if (keys.indexOf(Keyboard.right) != -1)
-            vec3.subtract(this.getPos(), this.getPos(), vec3.fromValues(0.1, 0.0, 0.0)); // right
+            vec3.subtract(this.getPos(), this.getPos(), vec3.fromValues(this.speed, 0.0, 0.0)); // right
         if (keys.indexOf(Keyboard.up) != -1)
-            vec3.add(this.getPos(), this.getPos(), vec3.fromValues(0.0, 0.0, 0.1)); // forward
+            vec3.add(this.getPos(), this.getPos(), vec3.fromValues(0.0, 0.0, this.speed)); // forward
         if (keys.indexOf(Keyboard.down) != -1)
-            vec3.subtract(this.getPos(), this.getPos(), vec3.fromValues(0.0, 0.0, 0.1)); // backward
+            vec3.subtract(this.getPos(), this.getPos(), vec3.fromValues(0.0, 0.0, this.speed)); // backward
     }
 
     /**
